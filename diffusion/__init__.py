0c897ddfc0f2ffa2d5b2d83b248ef05a2ef2d59b
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-06-20 23:47:05 +0900

    Update __init__.py

--- diffusion/__init__.py
+++ diffusion/__init__.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('./')
 from .Test import Test, f1
-from .Functions import Functions
+from .DMFunctions import DMFunctions
 from .Network import Unet
 from .Scheduler import Scheduler
