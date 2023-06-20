5faf4d3d2efee145a3e3a8941d13b74bd46568bf
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-06-20 21:57:45 +0900

    Update and rename UNet.py to Network.py

--- diffusion/Network.py
+++ diffusion/Network.py
@@ -1,11 +1,41 @@
+#@title Network
+import math
+from inspect import isfunction
+from functools import partial
+
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
 import torch
 from torch import nn, einsum
 import torch.nn.functional as F
+
+
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
 
 class SinusoidalPositionEmbeddings(nn.Module):
     def __init__(self, dim):
         super().__init__()
         self.dim = dim
+        self.fn = Functions(10)
 
     def forward(self, time):
         device = time.device
@@ -15,7 +45,6 @@ class SinusoidalPositionEmbeddings(nn.Module):
         embeddings = time[:, None] * embeddings[None, :]
         embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
         return embeddings
-
 
 
 class WeightStandardizedConv2d(nn.Conv2d):
@@ -279,25 +308,25 @@ class Unet(nn.Module):
         return self.final_conv(x)
 
 
-    class Residual(nn.Module):
-        def __init__(self, fn):
-            super().__init__()
-            self.fn = fn
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
 
-        def forward(self, x, *args, **kwargs):
-            return self.fn(x, *args, **kwargs) + x
-
-
-    def Upsample(dim, dim_out=None):
-        return nn.Sequential(
-            nn.Upsample(scale_factor=2, mode="nearest"),
-            nn.Conv2d(dim, default(dim_out, dim), 3, padding=1),
-        )
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
 
 
-    def Downsample(dim, dim_out=None):
-        # No More Strided Convolutions or Pooling
-        return nn.Sequential(
-            Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
-            nn.Conv2d(dim * 4, default(dim_out, dim), 1),
-        )
+def Upsample(dim, dim_out=None):
+    return nn.Sequential(
+        nn.Upsample(scale_factor=2, mode="nearest"),
+        nn.Conv2d(dim, default(dim_out, dim), 3, padding=1),
+    )
+
+
+def Downsample(dim, dim_out=None):
+    # No More Strided Convolutions or Pooling
+    return nn.Sequential(
+        Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
+        nn.Conv2d(dim * 4, default(dim_out, dim), 1),
+    )
