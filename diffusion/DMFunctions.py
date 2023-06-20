02c25e1f21dbdaa54439e6d7d73e3ae19bb0818e
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-06-21 06:36:12 +0900

    Update DMFunctions.py

--- diffusion/DMFunctions.py
+++ diffusion/DMFunctions.py
@@ -46,6 +46,9 @@ class DMFunctions():
         if t:
             text = "Step:" + str(t)
             plt.text(0, 0, text, fontdict=None, bbox=dict(facecolor='white', alpha=1))
+        ax = plt.gca()
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False
         plt.imshow(noisy_image)
         plt.show()
 
